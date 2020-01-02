80bd2722d991ef9ad9788e148044156859726a9f
Eric Stockenstrom <eric@zs6buj.com>
2020-01-02 09:54:53 +0200

    Sloppy exits removed. :)

--- AntTrack_v2.14/AntTrack_v2.14.ino
+++ AntTrack_v2.14/AntTrack_v2.14.ino
@@ -5,13 +5,25 @@
     Eric Stockenstrom - Original code June 2017  
                         Version 2 March 2019
 
-    This program is free software. You may redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation. See here <http://www.gnu.org/licenses>
+  License and Disclaimer
 
-    The application was written in the hope that it will be useful, but it comes
-    without any warranty or implied warranty of merchantability or fitness 
-    for a particular purpose 
+ 
+  This software is provided under the GNU v2.0 License. All relevant restrictions apply including 
+  the following: In case there is a conflict, the GNU v2.0 License is overriding. This software is 
+  provided as-is in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
+  Public License for more details. In no event will the authors and/or contributors be held liable
+  for any damages arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose, including commercial 
+  applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software.
+  2. If you use this software in a product, an acknowledgment in the product documentation would be appreciated.
+  3. Altered versions must be plainly marked as such, and must not be misrepresented as being the original software.
+  4. This notice may not be removed or altered from any distribution.  
+
+  By downloading this software you are agreeing to the terms specified in this page and the spirit of thereof.
     
    *****************************************************************************
    Boards supported:
